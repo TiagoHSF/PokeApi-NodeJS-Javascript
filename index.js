a0f698f04b50c1a1710b9da4e3261a2ffe57c9e7
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-05-19 20:39:13 -0300

    Interação POKE API - usando NODE E JS

---
+++ index.js
@@ -0,0 +1,34 @@
+const api = require('./api')
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(8000)
+
+server.get('/', (req, res) => {
+    return res.send({message: "Tiago"})
+})
+
+server.get('/pokemon/', async (req, res) => {
+    try {
+        const { data } = await api.get('pokemon/1');
+        return res.send({ 
+            name: data.name
+        })
+    } catch (error) {
+        res.send({error: error.message})
+    }
+})
+
+server.get('/pokemon/todos', async (req, res) => {
+    try {
+        const { data } = await api.get('pokemon/');
+        return res.send({ 
+            data
+        })
+    } catch (error) {
+        res.send({error: error.message})
+    }
+})
